0e9db2707627b0b13e73d48dff014fd3d5a01d76
J0kerul <a-marinescu@outlook.de>
2026-01-20 00:18:51 +0100

    refactor: changed function signatures in interface for taskrepo

--- backend/internal/task/task_interface.go
+++ backend/internal/task/task_interface.go
@@ -14,14 +14,14 @@ type TaskFilter struct {
 }
 
 type TaskRepositoryInterface interface {
-	CreateTask(ctx context.Context, task *Task) error
-	UpdateTask(ctx context.Context, task *Task) error
-	GetTaskById(ctx context.Context, taskid uuid.UUID) (*Task, error)
-	GetAllTasks(ctx context.Context) ([]*Task, error)
-	GetTaskByFilter(ctx context.Context, filter TaskFilter) ([]*Task, error)
-	SearchTasks(ctx context.Context, query string) ([]*Task, error)
-	DeleteTask(ctx context.Context, taskid uuid.UUID) error
-	BulkDeleteTasks(ctx context.Context, taskids []uuid.UUID) error
+	Create(ctx context.Context, task *Task) error
+	Update(ctx context.Context, task *Task) error
+	GetById(ctx context.Context, taskid uuid.UUID) (*Task, error)
+	GetAll(ctx context.Context) ([]*Task, error)
+	GetByFilter(ctx context.Context, filter TaskFilter) ([]*Task, error)
+	Search(ctx context.Context, query string) ([]*Task, error)
+	Delete(ctx context.Context, taskid uuid.UUID) error
+	BulkDelete(ctx context.Context, taskids []uuid.UUID) error
 	ToggleStatus(ctx context.Context, taskid uuid.UUID) error
 }
 
